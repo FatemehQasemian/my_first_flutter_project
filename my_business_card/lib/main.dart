49c3f6e36a66a8799afc4524da9ef48ef0cc8ff2
FatemehQasemian <ghasemian29fatemeh@gmail.com>
2022-01-23 18:54:05 +0330

    Use the card widget instead of the Container widget

--- my_business_card/lib/main.dart
+++ my_business_card/lib/main.dart
@@ -1,8 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,34 +12,77 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children:  <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/MyPic.jpg'),
-              ),
-              Text(
-                'Fatemeh Qasemian',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'Pacifico',
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.bold,
+        body: Center(
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/MyPic.jpg'),
                 ),
-              ),
-              Text(
-                'FLUTTER DEVELOPER',
-                style: TextStyle(
-                  color: Colors.teal.shade100,
-                  fontFamily: 'Source Sans Pro',
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2.5,
+                const Text(
+                  'Fatemeh Qasemian',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-            ],
+                Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    color: Colors.teal.shade100,
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                  child: ListTile(
+                    leading: const Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+1 234 567 895',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: 'Source San Pro',
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                  child: ListTile(
+                    leading: const Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'qasemianf@gmail.com',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: 'Source San Pro',
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
